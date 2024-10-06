699f62cfbf4d08fd9c8fd44bddee98732d3df743
Andrea-Coa <andrea.coa@utec.edu.pe>
2024-10-06 16:21:08 -0500

    api calls work

--- api.js
+++ api.js
@@ -1,23 +1,37 @@
 // llamadas a las apis
-const axios = require("axios")
+const axios = require("axios");
+const { response } = require("express");
 
 // modificar con los valores reales
-const api_cursos = "http://3.94.250.107:7999";
-const api_profesores = "api-profesores_c:8080";
-const api_estudiantes = "api-estudiantes_c:8000";
+const api_cursos = "http://api-cursos_c:7999";
+const api_estudiantes = "http://api-estudiantes_c:7998";
+const api_profesores = "api-profesores_c:7997";
 
 // get todos los cursos del alumno en el periodo actual
 // todavía no funciona con periodo
-const getCursoByAlumnoByPeriodo = async(correo, periodo="2024-2") => {
+const getCursoByAlumnoByPeriodo = async(correo, periodo) => {
     try {
-        response = await axios.get(`${api_cursos}/inscripciones/alumno/${correo}`);
+        const response = await axios.get(`${api_cursos}/inscripciones/alumno/${correo}/${periodo}`);
         return response.data;
     }
     catch (error) {
         console.error(error);
     }
-}
+};
+
+// GET datos del alumno BY CORREO
+const getAlumnoDataByCorreo = async(correo) => {
+    try {
+        const response = await axios.get(`${api_estudiantes}/api/estudiantes/by-email?correo=${correo}`);
+        return response.data;
+    }
+    catch (error) {
+        console.error(error);
+        return {};
+    }
+};
 
 module.exports = {
-    getCursoByAlumnoByPeriodo
+    getCursoByAlumnoByPeriodo,
+    getAlumnoDataByCorreo
 };
--- routes/matricula.js
+++ routes/matricula.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { getCursoByAlumnoByPeriodo } = require("../api");
+const { getCursoByAlumnoByPeriodo, getAlumnoDataByCorreo } = require("../api");
 
 // Get all cursos by CORREO ALUMNO and PERIODO
-router.get("/:correo_alumno", async(req, res) => {
-    console.log("inside get");
+// + DATA de ALUMNO
+router.get("/:correo_alumno/:periodo", async(req, res) => {
     try {
-        const cursos = await getCursoByAlumnoByPeriodo(req.params.correo_alumno);
-        console.log(cursos);
-        res.json(cursos);
+        let cursos_ = await getCursoByAlumnoByPeriodo(req.params.correo_alumno, req.params.periodo);
+
+        let data_alumno_ = await getAlumnoDataByCorreo(req.params.correo_alumno);
+        let {nombre, correo, carrera, escalaPago } = data_alumno_;
+        data_alumno_ = {nombre, correo, carrera, escalaPago};
+        console.log(cursos_);
+        
+        const unified_response = {
+            data_alumno: data_alumno_,
+            cursos: cursos_
+        };
+        res.json(unified_response);
     }
     catch (err) {
         return res.status(500).json({ message: err.message});
